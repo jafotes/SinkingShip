dda2a9f9f3fd9591ffe4bec306bb85f5dd743c63
Magnesiam <jorgep.s.matos@gmail.com>
2018-02-26 22:31:23 +0000

    Added navbar, and countdown

--- server.js
+++ server.js
@@ -32,6 +32,37 @@ app.post('/', function (req, res) {
     res.redirect('/');
 })
 
+
+app.get('/countdown', function (req, res) {
+    var json = JSON.parse(fs.readFileSync('Countdown.json', 'utf8'));
+    let date_time = json[0].datetime;
+
+    res.render('countdown', { date: date_time });
+})
+
+app.post('/countdown', function (req, res) {
+    var json = JSON.parse(fs.readFileSync('Countdown.json', 'utf8'));
+    let date_time = json[0].datetime;
+
+    res.redirect('/countdown');
+})
+
+app.get('/leaderboard', function (req, res) {
+    res.render('leaderboard', {});
+})
+
+app.post('/leaderboard', function(req, res) {
+    res.redirect('/leaderboard')
+})
+
+app.get('/about', function (req, res) {
+    res.render('about', {});
+})
+
+app.post('/about', function (req, res) {
+    res.redirect('/about')
+})
+
 app.listen(port, function () {
     console.log('Example app listening on port ' + port + '!')
 })
