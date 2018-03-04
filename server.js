40429cf58d637c44d3731404a5de73077a358fc8
Magnesiam <jorgep.s.matos@gmail.com>
2018-03-04 00:21:58 +0000

    Added Leaderboard, about and changed countdown to countdown list. Needs major refactoring

--- server.js
+++ server.js
@@ -26,9 +26,10 @@ app.get('/', function (req, res) {
 
 app.get('/countdown', function (req, res) {
     var json = JSON.parse(fs.readFileSync('Countdown.json', 'utf8'));
-    let date_time = json[0].datetime;
+    let date_time1 = json[0].datetime;
+    let date_time2 = json[1].datetime;
 
-    res.render('countdown', { date: date_time });
+    res.render('countdown', { date1: date_time1, date2: date_time2 });
 })
 
 /* app.post('/countdown', function (req, res) {
@@ -39,7 +40,33 @@ app.get('/countdown', function (req, res) {
 }) */
 
 app.get('/leaderboard', function (req, res) {
-    res.render('leaderboard', {});
+    var json = JSON.parse(fs.readFileSync('Resets.json', 'utf8'));
+    var alldates = [];
+    var alldates_formated = [];
+    var len = Object.keys(json).length;
+
+    for (var i = 1; i < len; i++) {
+        var date1 = new Date(json[i - 1].datetime);
+        var date2 = new Date(json[i].datetime);
+        var datedelta = date1 - date2;
+        alldates.push(datedelta)
+    }
+
+    alldates.sort(function (a, b) {
+        var c = new Date(a.date);
+        var d = new Date(b.date);
+        return c - d;
+    });
+
+    for (var i = alldates.length - 1; i >= 0; i--) {
+        days = Math.floor(alldates[i] / (60 * 60 * 1000 * 24) * 1);
+        hours = Math.floor((alldates[i] % (60 * 60 * 1000 * 24)) / (60 * 60 * 1000) * 1);
+        formated_days = ("0" + days).slice(-2);
+        formated_hours = ("0" + hours).slice(-2);
+        alldates_formated.push([formated_days, formated_hours]);
+    }
+
+    res.render('leaderboard', { alldates: alldates_formated });
 })
 
 /* app.post('/leaderboard', function (req, res) {
